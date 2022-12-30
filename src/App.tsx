a439b8845a4db9d293281c76ad33137e442221c1
Thomas Matysik <thomas@matysik.co.nz>
2022-12-30 13:07:04 +1300

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
+
+function Home() {
+  return <p>This is the home page.</p>;
+}
+
+function Bookmark() {
+  return <p>Bookmark page</p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar
+          title="Capture"
+          items={[
+            { label: 'Home', to: '/' },
+            { label: 'Capture', to: '/capture' },
+          ]}
+        />
+        <Routes>
+          <Route path="/capture" element={<Bookmark />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import styles from './Navbar.module.css';
+
+function HamburgerIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path
+        stroke="black"
+        stroke-width="2"
+        d="M3 5 L17 5 M3 10 L17 10 M3 15 L17 15"
+      />
+    </svg>
+  );
+}
+
+function CrossIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+      <path stroke="black" stroke-width="2" d="M5 5 L15 15 M15 5 L5 15" />
+    </svg>
+  );
+}
+
+function Hamburger({ isOpen }: { isOpen: boolean }) {
+  return isOpen ? <CrossIcon /> : <HamburgerIcon />;
+}
+
+export default function Navbar({
+  title,
+  items,
+}: {
+  title: string;
+  items: { label: string; to: string }[];
+}) {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  return (
+    <nav className={styles.navigation}>
+      <span className={styles.brandName}>{title}</span>
+      <button
+        className={styles.hamburger}
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        <Hamburger isOpen={isNavExpanded} />
+      </button>
+      <ul
+        className={`${styles.navigationMenu} ${
+          isNavExpanded ? styles.expanded : ''
+        }`}
+      >
+        {items.map((item) => (
+          <li>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.navLinkActive : undefined
+              }
+              to={item.to}
+            >
+              {item.label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
